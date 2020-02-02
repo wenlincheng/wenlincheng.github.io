5fe3988726fc5a5751c222151468486e75a970df
wenlincheng <lincheng.wen@daddylab.com>
2020-02-02 17:25:23 +0800

    Site updated: 2020-02-02 17:25:22

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Sun Feb 02 2020 17:14:51 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
